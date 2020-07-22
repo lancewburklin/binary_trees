465599422a7af8fa088a6a4a23faba27e019d051
lancewburklin <1677@holbertonschool.com>
2020-07-22 17:43:22 +0000

    Properly deletes a binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes a binary tree
+ * @tree: The tree being deleted
+ *
+ * Return: Void
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		free (tree);
+		return;
+	}
+	if (tree->left != NULL)
+		binary_tree_delete(tree->left);
+	if (tree->right != NULL)
+		binary_tree_delete(tree->right);
+	if (tree != NULL)
+		free (tree);
+	return;
+}
