c0359b2858881b92eb45f2c6e3d49934b6edbcb2
lancewburklin <1677@holbertonschool.com>
2020-07-23 21:15:44 +0000

    Now checks number of leaves and compares

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,6 +13,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
 		return (0);
+	if (binary_tree_leaves(tree->left) != binary_tree_leaves(tree->right))
+		return (0);
+	if (binary_tree_leaves(tree->left) % 2 != 0)
+		return (0);
 	return (binary_tree_is_full(tree));
 }
 
@@ -64,4 +68,24 @@ else
 if (num > num_right)
 	return (num);
 return (num_right);
+}
+
+/**
+ * binary_tree_leaves - Gets leaves from a binary tree
+ * @tree: Finds the leaves of a tree
+ *
+ * Return: Number of leaves or 0 if none
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t num_of_leaves = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	num_of_leaves += binary_tree_leaves(tree->right) +
+		binary_tree_leaves(tree->left);
+	return (num_of_leaves);
 }
