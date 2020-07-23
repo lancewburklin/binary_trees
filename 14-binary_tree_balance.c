3ca97c9831e239162f49a518eb07e4fbfb3e3e49
lancewburklin <1677@holbertonschool.com>
2020-07-23 00:58:23 +0000

    Fixed issues

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,18 +9,43 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int bal = 1;
-	int bal_right = 1;
+	int bal = 0;
+	int bal_right = 0;
 
 	if (tree == NULL)
 		return (0);
 	if (tree->left != NULL)
-		bal += binary_tree_balance(tree->left);
-	else
-		bal = 0;
+		bal = 1 + binary_tree_height(tree->left);
 	if (tree->right != NULL)
-		bal_right += binary_tree_balance(tree->right);
-	else
-		bal_right = 0;
+		bal_right = 1 + binary_tree_height(tree->right);
 	return (bal - bal_right);
+}
+
+/**
+ * binary_tree_height - Finds the height of a binary tree at a node
+ * @tree: Tree to find the height of
+ *
+ * Return: The height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t num = 0;
+	size_t num_right = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left)
+		num = 1 + binary_tree_height(tree->left);
+	else
+		num = 0;
+	if (tree->right)
+		num_right = 1 + binary_tree_height(tree->right);
+	else
+		num_right = 0;
+	if (num > num_right)
+		return (num);
+	return (num_right);
 }
