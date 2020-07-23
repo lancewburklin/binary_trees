cebe285da9655a1744e99aac1cc6d5e6cf9bfd7c
lancewburklin <1677@holbertonschool.com>
2020-07-23 21:20:12 +0000

    Removed excess function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -17,28 +17,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (binary_tree_leaves(tree->left) % 2 != 0)
 		return (0);
-	return (binary_tree_is_full(tree));
-}
-
-/**
- * binary_tree_is_full - Tests if a binary tree is full
- * @tree: The tree to test
- *
- * Return: 1 if full, otherwise 0
- */
-
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	if (tree->right == NULL && tree->left == NULL)
-		return (1);
-	if (tree->left != NULL && tree->right != NULL)
-	{
-		return (binary_tree_is_full(tree->left) &&
-			binary_tree_is_full(tree->right));
-	}
-	return (0);
+	return (1);
 }
 
 /**
